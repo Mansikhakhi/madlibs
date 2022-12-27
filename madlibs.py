e2076e7129c64f86467b76da3f36b2cfabd672d0
Mansikhakhi <106768496+Mansikhakhi@users.noreply.github.com>
2022-12-27 14:30:10 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,7 @@
+what = input("First blanck: ")
+hobby = input("Verb 1: ")
+famous_person = input("Famous person: ")
+crush = input("Write mansi's crush name : ")
+
+madlibs = f"Mansi is so {what}!. She loves doing {hobby}. Shine like a star like you are {famous_person}! SHe has a crush on {crush}."
+print(madlibs)
